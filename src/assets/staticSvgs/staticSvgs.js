cfbf66eeef6584c57649c518ccac3d792033258e
pollman <caleb.pollman@zeeram.com>
2017-10-12 11:51:57 -0600

    added svg

--- src/assets/staticSvgs/staticSvgs.js
+++ src/assets/staticSvgs/staticSvgs.js
@@ -1,13 +1,10 @@
 const staticSvgs = {
-  'test time': require('./svgFiles/headOnANail.svg'),
+  'head on a nail': require('./svgFiles/headOnANail.svg'),
+  'march 16': require('./svgFiles/whatsTheDealWithTheseDots.svg'),
   'previous luxury': require('./svgFiles/everyPathNeedsAPoint.svg'),
-  'march 2016': require('./svgFiles/whatsTheDealWithTheseDots.svg'),
+  'repetition': require('./svgFiles/thatRepetition.svg'),
   'that water': require('./svgFiles/thatWater.svg'),
-  'small': require('./svgFiles/smallRed.svg'),
-  'medium': require('./svgFiles/smallBlackTest.svg'),
-  'large': require('./svgFiles/testSvg.svg'),
   'docker': require('./svgFiles/docker.svg'),
-  'multiGroup': require('./svgFiles/copySmallRed.svg'),
 };
 
 export default staticSvgs;
